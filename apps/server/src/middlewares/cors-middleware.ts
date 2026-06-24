9c20489d6c3d17e53d1dd8c35f1f018be35a7595
fgbyte <fer.garciabayon@gmail.com>
2026-06-24 10:39:27 -0400

    Update cors-middleware.ts

--- apps/server/src/middlewares/cors-middleware.ts
+++ apps/server/src/middlewares/cors-middleware.ts
@@ -2,7 +2,7 @@ import { cors } from "hono/cors";
 import { env } from "@generai/env/server";
 
 export const corsMiddleware = cors({
-  origin: env.CORS_ORIGIN, //alchemy envs
+  origin: () => env.CORS_ORIGIN, // read lazily per-request so env bindings are guaranteed to be populated
   allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowHeaders: ["Content-Type", "Authorization"],
   credentials: true,
